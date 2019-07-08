84ba72ec31177bc24d6f1efc8f3587eead6b1a7e
Illya Suzyma <illya.suzyma@gmail.com>
2019-07-08 13:01:00 +0300

    Added implementations of repositories to accounts and authentication

---
+++ Authentication.Data/Repositories/Repository.cs
@@ -0,0 +1,84 @@
+﻿using Common.Core.Models;
+using DocumentLight.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        public T Add(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> AddAsync(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<T> obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindBy<T2>(Expression<Func<T, bool>> predicate, params Expression<Func<T, T2>>[] paths)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAllWithIncludies(params Expression<Func<T, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByIdWithIncludies(Guid id, params Expression<Func<T, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Authentication.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Common.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
