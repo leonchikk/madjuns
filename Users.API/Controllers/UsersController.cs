eaabcc10c071b7ae342fca94c4970a9a030bc586
Elijah <illya.suzyma@gmail.com>
2019-08-13 22:40:44 +0300

    Added users controller

--- Users.API/Controllers/UsersController.cs
+++ Users.API/Controllers/UsersController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Users.API.Interfaces;
+using Users.API.Models.Requests;
 
 namespace Users.API.Controllers
 {
@@ -6,6 +10,48 @@ namespace Users.API.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUsersService _usersService;
 
+        public UsersController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(_usersService.GetUsers());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(Guid id)
+        {
+            return Ok(_usersService.GetUserById(id));
+        }
+
+        [HttpGet("{id}/profile")]
+        public IActionResult GetUserProfileById(Guid id)
+        {
+            return Ok(_usersService.GetUserProfile(id));
+        }
+
+        [HttpGet("{id}/setting")]
+        public IActionResult GetUserSettingsById(Guid id)
+        {
+            return Ok(_usersService.GetUserSettings(id));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateUserRequest request)
+        {
+            return Ok(await _usersService.UpdateUserAsync(id, request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _usersService.DeleteUserAsync(id);
+            return Ok();
+        }
     }
 }
