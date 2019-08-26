aa40634718ef4bf145085050e357d203f343979d
Elijah <illya.suzyma@gmail.com>
2019-08-26 15:37:10 +0300

    Added migrations

---
+++ Users.Data/Migrations/20190826122924_RemovedAccountId.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Users.Data.Migrations
+{
+    public partial class RemovedAccountId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AccountId",
+                table: "Users",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
