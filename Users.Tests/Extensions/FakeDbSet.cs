a45d3ac0f834608ad09ae88c6532c8a71d43741e
Elijah <illya.suzyma@gmail.com>
2019-09-22 22:29:19 +0300

    Remade users.tests project and added one really working unit test ;)

---
+++ Users.Tests/Extensions/FakeDbSet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Users.Tests.Extensions
+{
+    public class FakeDbSet<T> : DbSet<T> where T : class
+    {
+        ObservableCollection<T> _data;
+        IQueryable _query;
+
+        public FakeDbSet()
+        {
+            _data = new ObservableCollection<T>();
+            _query = _data.AsQueryable();
+        }
+
+        public virtual T Find(params object[] keyValues)
+        {
+            throw new NotImplementedException("Derive from FakeDbSet<T> and override Find");
+        }
+
+        public T Add(T item)
+        {
+            _data.Add(item);
+            return item;
+        }
+
+        public T Remove(T item)
+        {
+            _data.Remove(item);
+            return item;
+        }
+
+        public T Attach(T item)
+        {
+            _data.Add(item);
+            return item;
+        }
+
+        public T Detach(T item)
+        {
+            _data.Remove(item);
+            return item;
+        }
+
+        public T Create()
+        {
+            return Activator.CreateInstance<T>();
+        }
+
+        public TDerivedEntity Create<TDerivedEntity>() where TDerivedEntity : class, T
+        {
+            return Activator.CreateInstance<TDerivedEntity>();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate)
+        {
+            return _data.AsQueryable().Where(predicate);
+        }
+
+        public ObservableCollection<T> Local
+        {
+            get { return _data; }
+        }
+    }
+}
--- Users.Tests/Services/UsersServiceTests.cs
+++ Users.Tests/Services/UsersServiceTests.cs
@@ -1,47 +1,66 @@
-﻿using AutoMapper;
+﻿using AutoFixture;
 using Common.Core.Interfaces;
-using EasyNetQ;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
 using Users.API.Interfaces;
 using Users.API.Services;
 using Users.Core.Domain;
+using Users.Data;
+using Users.Data.Repositories;
+using Users.Tests.Extensions;
 
 namespace Users.Tests.Services
 {
     [TestClass]
     public class UsersServiceTests
     {
-        [TestMethod]
-        public void CheckDeleteUser_DeleteUserWithValidId_OnSuccess()
-        {
-            var userId = Guid.NewGuid();
-            var userServiceMock = new Mock<IUsersService>();
+        private Guid _accountId = Guid.Empty;
 
-            userServiceMock.Setup(x => x.DeleteUserAsync(userId)).Verifiable();
+        private Mock<Repository<User>> _mockUsersRepository;
+
+        private readonly Fixture _fixture;
+        private IUsersService _usersService;
+
+        public UsersServiceTests()
+        {
+            _fixture = new Fixture();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _accountId = _fixture.Create<Guid>();
+
+            var testData = new List<User>
+            {
+                new User(_accountId, null)
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<User>>();
+            mockSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(testData.Provider);
+            mockSet.As<IQueryable<User>>().Setup(m => m.Expression).Returns(testData.Expression);
+            mockSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(testData.ElementType);
+            mockSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(testData.GetEnumerator());
+
+            var mockContext = new Mock<UsersContext>();
+            mockContext.Setup(c => c.Set<User>()).Returns(mockSet.Object);
+
+            _mockUsersRepository = new Mock<Repository<User>>(mockContext.Object);
+
+            _usersService = new UsersService(null, null, null, _mockUsersRepository.Object);
         }
 
         [ExpectedException(typeof(Exception))]
         [TestMethod]
-        public void CheckGetUserById_GetUserWithNotExistId_OnFailed()
+        public void GetUserById_IncorrectId_ShouldReturn_Exception()
         {
-            var userIdToCheck = Guid.NewGuid();
-            var userEntryId = Guid.NewGuid();
-
-            var userEntries = new List<User>() { new User(userEntryId, null) }.AsQueryable();
-
-            var userRepositoryMock = new Mock<IRepository<User>>();
-            var mapperMock = new Mock<IMapper>();
-
-            userRepositoryMock.Setup(x => x.FindBy(It.IsAny<Expression<Func<User, bool>>>())).Returns(userEntries);
-
-            var userService = new UsersService(null, null, mapperMock.Object, userRepositoryMock.Object);
-            userService.GetUserById(userIdToCheck);
+            var invalidAccountId = _fixture.Create<Guid>();
+             _usersService.GetUserById(invalidAccountId);
         }
     }
 }
