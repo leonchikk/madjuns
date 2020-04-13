3f616001a2e9ceddefbca7c155afacfaa1b67734
Elijah <illya.suzyma@gmail.com>
2020-04-13 14:26:31 +0300

    Added Delete and Undelete method to BaseEntity

--- Common.Core/Models/BaseEntity.cs
+++ Common.Core/Models/BaseEntity.cs
@@ -6,5 +6,15 @@ namespace Common.Core.Models
     {
         public Guid Id { get; set; }
         public bool IsDeleted { get; set; }
+
+        public void Delete()
+        {
+            IsDeleted = true;
+        }
+
+        public void UnDelete()
+        {
+            IsDeleted = false;
+        }
     }
 }
